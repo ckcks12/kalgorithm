483815cbfd105ce2ee501104e2012bc0ad62e5e2
dlwlgjs132 <36119144+dlwlgjs132@users.noreply.github.com>
2019-09-23 17:42:01 +0900

    Create (2day)11650.cpp

---
+++ jiheon/(2day)11650.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+#define xx first
+#define yy second
+#define sz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define init(x, y) memset(x, y, sizeof(x))
+#define endl '\n'
+#define fastio                   \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr)
+#define f(n, x, y) for (int(n) = (x); (n) < (y); (n)++)
+#define f0(n, x) for (int (n) = 0; (n) < (x); (n)++)
+#define input(x) \
+    int(x);      \
+    cin >> x;
+#define push emplace_back
+#define ll long long
+
+using namespace std;
+
+vector<pair<int, int> > parr;
+vector<pair<int, int> >::iterator iter;
+
+int main(){
+    input(N);
+
+    f0(i, N){
+        input(x);
+        input(y);
+        
+        parr.push_back(pair<int, int>(x,y));
+    }
+    sort(parr.begin(), parr.end());
+    for(iter=parr.begin();iter != parr.end();iter++){
+        cout<<iter->first<<" "<<iter->second<<endl;
+    }
+}
